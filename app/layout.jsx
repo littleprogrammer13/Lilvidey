c8e66e707fbf4809cdd336c78c4e0733a7adc101
littleprogrammer13 <buxybux0@gmail.com>
2025-11-18 08:30:27 -0300

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Video AI Studio",
+  description: "Studio de Vídeo AI",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
