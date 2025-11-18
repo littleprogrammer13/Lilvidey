b4ae7a3b568d2c25393cfaf3ec6fab99b6e45921
littleprogrammer13 <buxybux0@gmail.com>
2025-11-18 08:26:15 -0300

    Update fetchFrame.js

--- utils/fetchFrame.js
+++ utils/fetchFrame.js
@@ -1,13 +1,7 @@
 export async function fetchFrame(prompt) {
-  const url = `https://hf.space/embed/akhaliq/ai-image-generation-demo/+/api/predict/?prompt=${encodeURIComponent(
-    prompt
-  )}`;
-
+  const url = `https://hf.space/embed/akhaliq/ai-image-generation-demo/+/api/predict/?prompt=${encodeURIComponent(prompt)}`;
   const res = await fetch(url);
   const data = await res.json();
-
-  // API retorna base64 sem header
   const base64 = data.data[0];
-
   return Buffer.from(base64, "base64");
 }
